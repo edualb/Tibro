13107a935c68c146d3eaa315552481d955bf48af
Arthur Silva <arxdsilva@gmail.com>
2018-12-12 14:12:33 -0200

    test: add simple unit tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,25 @@
+const ops = require('../buscaMonstros.js');
 const assert = require('assert');
 
-it('should return true', () => {
-  assert.equal(true, true);
-}); 
+// not the best solution, it should resolve the promise first,
+// mount an object and then test each field
+describe('tests', async () => {
+    var monstro = {};
+    const url = "https://playragnarokonlinebr.com/database/thor/monstros/detalhes/poporing";
+    describe('test monster details', () => {
+        it('monster should not be empty', () => {
+            return ops.buscaDetalhesMstr(url)
+                .then((data) => {
+                    assert(data != null, "monster is empty");
+                }
+            )
+        })
+        it('monster should have name', () => {
+            return ops.buscaDetalhesMstr(url)
+                .then((data) => {
+                    assert.equal(data.nome, "Poporing");
+                }
+            )
+        });
+    })
+});
