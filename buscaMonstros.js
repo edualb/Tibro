11502dcdf33461a7f1a111b954e2a4097e8b3291
edualb <eduardoslafiq@gmail.com>
2018-12-12 01:55:56 -0200

    Retornando o objeto inteiro. Esta solução está bem mais rápida que a anterior, pois passei a passar o body da página ao inves da url... TODO: Estamos agora com um problema de assincronidade. Está ocorrendo que o forEach do exemplo que vamos utilizar no discord está mandando links mais rapido do que deveria. Uma solução seria usar um sleep, mas não acho recomendavel, logo, temos que buscar uma solução melhor.

--- buscaMonstros.js
+++ buscaMonstros.js
@@ -5,15 +5,18 @@ const urlThor = 'https://playragnarokonlinebr.com/database/thor/monstros?page=1&
   urlDetalhesThor = 'https://playragnarokonlinebr.com/database/thor/',
   urlValhalla = 'https://playragnarokonlinebr.com/database/valhalla/monstros?page=1&nome=',
   urlDetalhesValhalla = 'https://playragnarokonlinebr.com/database/valhalla/';
-const monstros = [];
 const links = [];
 
 // Exemplo de busca do monstro Poring no servidor Thor
-buscaLinksDetalhes("abelha", 0)
+buscaLinksDetalhes("poring", 0)
   .then(
     function() {
-      links.forEach(element => {
-        buscaDetalhesDoMonstro(element.url);
+      links.forEach(function(element) {
+        buscaDetalhesDoMonstro(element.url)
+          .then(async function(monstro) {
+              console.log(monstro.nome); //Aqui vai editar a mensagem do discord para cada monstro.
+            }, err => console.log("Error:" + err)
+          ).catch(e => console.log("Error:" + e));
       });
     }, err => console.log("Error:" + err)
   ).catch(e => console.log("Error:" + e));
@@ -38,67 +41,56 @@ async function buscaLinksDetalhes(nome , servidor) { // Servidor: 0 = Thor, 1 = 
 }
 
 async function buscaDetalhesDoMonstro(url) {
-  let informacoes, fraquezasEResistencias,atributosCaracteristicas, atributosBuild, drops;
+  var nomeMonstro, imgMonstro;
   await rp(url, function(err, res, body) {
+    console.log(url);
     if (err) console.log("Error:" + err);
     var $ = cheerio.load(body);
-    let nome = $('#itemDescription').find('h1').text();
-    let img = $('.col-xs-10 #hidden img').attr('src');
-    buscaInformacoesDoMonstro(url).then(function(result) {
-      informacoes = result;
-      buscaResistenciasEFraquezasDoMonstro(url).then(function(result) {
-        fraquezasEResistencias = result;
-        buscaAtributosCaracteristicasDoMonstro(url).then(function(result) {
-          atributosCaracteristicas = result;
-          buscaAtributosBuildDoMonstro(url).then(function(result) {
-            atributosBuild = result;
-            buscaDropsDoMonstro(url).then(function(result){
-
-            }).catch(e => console.log(e));
-          }).catch(e => console.log(e));
-        }).catch(e => console.log(e));
-      }).catch(e => console.log(e));
-    }).catch(e => console.log(e));
+    nomeMonstro = $('#itemDescription').find('h1').text();
+    imgMonstro = $('.col-xs-10 #hidden img').attr('src');
+    monstro = Object.create({
+      nome: nomeMonstro,
+      link: url,
+      img: imgMonstro,
+      informacoes: buscaInformacoesDoMonstro(body),
+      fraquezasEResistencias: buscaResistenciasEFraquezasDoMonstro(body),
+      atributosCaracteristicas: buscaAtributosCaracteristicasDoMonstro(body),
+      atributosBuild: buscaAtributosBuildDoMonstro(body),
+      drops: buscaDropsDoMonstro(body)
+    });
   });
-  /*let monstro = Object.create({
-    nome: nomeMonstro,
-    link: url
-  })*/
-  //monstros.push(monstro);
+  return monstro
 }
 
-async function buscaInformacoesDoMonstro(url) {
+function buscaInformacoesDoMonstro(body) {
   var informacaoNivel, informacaoRaca, informacaoPropriedade, informacaoTamanho, informacaoExpBase, informacaoExpClasse;
-  await rp(url, function(err, res, body) {
-    if (err) console.log("Error:" + err);
-    var $ = cheerio.load(body);
-    let array = $('#informacoes-list li').map(function() {
-      return $(this).text().trim();
-    }).toArray();
-    array.forEach(function(element, index) {
-      switch(index) {
-        case 1:
-          informacaoNivel = parseInt(element);
-          break;
-        case 3:
-          informacaoRaca = element;
-          break;
-        case 5:
-          informacaoPropriedade = element;
-          break;
-        case 7:
-          informacaoTamanho = element;
-          break;
-        case 9:
-          informacaoExpBase = parseFloat(element);
-          break;
-        case 11:
-          informacaoExpClasse = parseFloat(element);
-          break;
-        default:
-          break;
-      }
-    });
+  var $ = cheerio.load(body);
+  let array = $('#informacoes-list li').map(function() {
+    return $(this).text().trim();
+  }).toArray();
+  array.forEach(function(element, index) {
+    switch(index) {
+      case 1:
+        informacaoNivel = parseInt(element);
+        break;
+      case 3:
+        informacaoRaca = element;
+        break;
+      case 5:
+        informacaoPropriedade = element;
+        break;
+      case 7:
+        informacaoTamanho = element;
+        break;
+      case 9:
+        informacaoExpBase = parseFloat(element);
+        break;
+      case 11:
+        informacaoExpClasse = parseFloat(element);
+        break;
+      default:
+        break;
+    }
     informacoesMonstro = Object.create({
       nivel: informacaoNivel,
       raca: informacaoRaca,
@@ -111,50 +103,47 @@ async function buscaInformacoesDoMonstro(url) {
   return informacoesMonstro;
 }
 
-async function buscaResistenciasEFraquezasDoMonstro(url) {
+function buscaResistenciasEFraquezasDoMonstro(body) {
   var RFNeutro, RFTerra, RFVento, RFSagrado, RFFantasma, RFAgua, RFFogo, RFVeneno, RFSombrio, RFMaldito;
-  await rp(url, function(err, res, body) {
-    if (err) console.log("Error:" + err);
-    var $ = cheerio.load(body);
-    let array = $('#property li').map(function() {
-      return $(this).text().trim();
-    }).toArray();
-    array.forEach(function(element, index) {
-      switch(index) {
-        case 1:
-          RFNeutro = element;
-          break;
-        case 3:
-          RFAgua = element;
-          break;
-        case 5:
-          RFTerra = element;
-          break;
-        case 7:
-          RFFogo = element;
-          break;
-        case 9:
-          RFVento = element;
-          break;
-        case 11:
-          RFVeneno = element;
-          break;
-        case 13:
-          RFSagrado = element;
-          break;
-        case 15:
-          RFSombrio = element;
-          break;
-        case 17:
-          RFFantasma = element;
-          break;
-        case 19:
-          RFMaldito = element;
-          break;
-        default:
-          break;
-      }
-    });
+  var $ = cheerio.load(body);
+  let array = $('#property li').map(function() {
+    return $(this).text().trim();
+  }).toArray();
+  array.forEach(function(element, index) {
+    switch(index) {
+      case 1:
+        RFNeutro = element;
+        break;
+      case 3:
+        RFAgua = element;
+        break;
+      case 5:
+        RFTerra = element;
+        break;
+      case 7:
+        RFFogo = element;
+        break;
+      case 9:
+        RFVento = element;
+        break;
+      case 11:
+        RFVeneno = element;
+        break;
+      case 13:
+        RFSagrado = element;
+        break;
+      case 15:
+        RFSombrio = element;
+        break;
+      case 17:
+        RFFantasma = element;
+        break;
+      case 19:
+        RFMaldito = element;
+        break;
+      default:
+        break;
+    }
     FraquezasEResistenciasMonstro = Object.create({
       neutro: RFNeutro,
       terra: RFTerra,
@@ -171,35 +160,32 @@ async function buscaResistenciasEFraquezasDoMonstro(url) {
   return FraquezasEResistenciasMonstro;
 }
 
-async function buscaAtributosCaracteristicasDoMonstro(url) {
+function buscaAtributosCaracteristicasDoMonstro(body) {
   var atributoHP, atributoAtaque, atributoAlcance, atributoPrecisao, atributoEsquiva;
-  await rp(url, function(err, res, body) {
-    if (err) console.log("Error:" + err);
-    var $ = cheerio.load(body);
-    let array = $('#two-flexbox .information .list li').map(function() {
-      return $(this).text().trim();
-    }).toArray();
-    array.forEach(function(element, index) {
-      switch(index) {
-        case 1:
-          atributoHP = element;
-          break;
-        case 3:
-          atributoAtaque = element;
-          break;
-        case 5:
-          atributoAlcance = element;
-          break;
-        case 7:
-          atributoPrecisao = element;
-          break;
-        case 9:
-          atributoEsquiva = element;
-          break;
-        default:
-          break;
-      }
-    });
+  var $ = cheerio.load(body);
+  let array = $('#two-flexbox .information .list li').map(function() {
+    return $(this).text().trim();
+  }).toArray();
+  array.forEach(function(element, index) {
+    switch(index) {
+      case 1:
+        atributoHP = element;
+        break;
+      case 3:
+        atributoAtaque = element;
+        break;
+      case 5:
+        atributoAlcance = element;
+        break;
+      case 7:
+        atributoPrecisao = element;
+        break;
+      case 9:
+        atributoEsquiva = element;
+        break;
+      default:
+        break;
+    }
     AtributosCaracteristicasDoMonstro = Object.create({
       hp: atributoHP,
       ataque: atributoAtaque,
@@ -211,44 +197,41 @@ async function buscaAtributosCaracteristicasDoMonstro(url) {
   return AtributosCaracteristicasDoMonstro;
 }
 
-async function buscaAtributosBuildDoMonstro(url) {
+function buscaAtributosBuildDoMonstro(body) {
   var atributoDEF, atributoVIT, atributoDEFM, atributoINT, atributoFOR, atributoDES, atributoAGI, atributoSOR;
-  await rp(url, function(err, res, body) {
-    if (err) console.log("Error:" + err);
-    var $ = cheerio.load(body);
-    let array = $('#two-flexbox #flex-outside .flex-4 li').map(function() {
-      return $(this).text().trim();
-    }).toArray();
-    array.forEach(function(element, index) {
-      switch(index) {
-        case 1:
-          atributoDEF = element;
-          break;
-        case 3:
-          atributoVIT = element;
-          break;
-        case 5:
-          atributoDEFM = element;
-          break;
-        case 7:
-          atributoINT = element;
-          break;
-        case 9:
-          atributoFOR = element;
-          break;
-        case 11:
-          atributoDES = element;
-          break;
-        case 13:
-          atributoAGI = element;
-          break;
-        case 15:
-          atributoSOR = element;
-          break;
-        default:
-          break;
-      }
-    });
+  var $ = cheerio.load(body);
+  let array = $('#two-flexbox #flex-outside .flex-4 li').map(function() {
+    return $(this).text().trim();
+  }).toArray();
+  array.forEach(function(element, index) {
+    switch(index) {
+      case 1:
+        atributoDEF = element;
+        break;
+      case 3:
+        atributoVIT = element;
+        break;
+      case 5:
+        atributoDEFM = element;
+        break;
+      case 7:
+        atributoINT = element;
+        break;
+      case 9:
+        atributoFOR = element;
+        break;
+      case 11:
+        atributoDES = element;
+        break;
+      case 13:
+        atributoAGI = element;
+        break;
+      case 15:
+        atributoSOR = element;
+        break;
+      default:
+        break;
+    }
     AtributosBuildDoMonstro = Object.create({
       def: atributoDEF,
       vit: atributoVIT,
@@ -263,27 +246,15 @@ async function buscaAtributosBuildDoMonstro(url) {
   return AtributosBuildDoMonstro;
 }
 
-async function buscaDropsDoMonstro(url) {
-  var drop, preco, img, nome;
-  await rp(url, function(err, res, body) {
-    if (err) console.log("Error:" + err);
-    var $ = cheerio.load(body);
-    //console.log($(this).find('.itens .show img').attr('src'));
-    let array = $('#slider-result ul li').map(function() {
-      nomeItem = $(this).find('.show h5').text().trim();
-
-      return {nome: nomeItem};
-    }).toArray();
-    array.forEach(element => {
-      console.log(element.nome);
-    });
-    /*AtributosCaracteristicasDoMonstro = Object.create({
-      hp: atributoHP,
-      ataque: atributoAtaque,
-      alcance: atributoAlcance,
-      precisão: atributoPrecisao,
-      esquiva: atributoEsquiva
-    })*/
-  });
-  //return AtributosCaracteristicasDoMonstro;
+function buscaDropsDoMonstro(body) {
+  let drops
+  var $ = cheerio.load(body);
+  drops = $('#slider-result ul li').map(function() {
+    nomeItem = $(this).find('.show h5').text().trim();
+    urlItem = $(this).find('img').attr('src');
+    dropItem = $(this).find('label').eq(1).text().trim();
+    precoItem = $(this).find('label').eq(2).text().trim();
+    return {nome: nomeItem, img: urlItem, dropChance: dropItem, preco: precoItem};
+  }).toArray();
+  return drops;
 }
