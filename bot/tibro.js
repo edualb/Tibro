696b780d7ef3e37c0583cff0c9e21bad62b98553
edualb <eduardoslafiq@gmail.com>
2018-12-18 19:02:53 -0200

    código extraido para uma função. Incluído o exemplo do servidor Valhalla no discord.

--- bot/tibro.js
+++ bot/tibro.js
@@ -4,7 +4,6 @@ const ops = require('../buscaMonstros.js');
 const bot = new discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-    bot.se
     bot.user.setActivity("Ragnarok Online");
 });
 
@@ -13,23 +12,30 @@ bot.on("message", async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-
     if(cmd === `${prefix}mobt`) {
-        ops.buscaLinks(ops.formataMsgBusca(args), 0).then(async function(links) {
-            for (const link of links) {
-              await ops.buscaDetalhesMstr(link.url)
-              .then(function(monstro) {
-                const embed = new discord.RichEmbed()
-                    .setTitle(monstro.nome)
-                    .setColor(0xFFA200)
-                    .setDescription('montar mensagem')
-                return message.channel.send(embed);
-                }, err => console.log("Error:" + err)
-              ).catch(e => console.log("Error:" + e));
-            }
-          }, err => console.log("Error:" + err)
-        ).catch(e => console.log("Error:" + e));
+        msgEmbededServidor(message, 0, args);
+    } else if(cmd === `${prefix}mobv`) {
+        msgEmbededServidor(message, 1, args);
     }
 });
 
 bot.login(tibroconfig.token);
+
+function msgEmbededServidor(message, servidor, nomeMonstro) {
+    let colorEmbeded = 0xFFA200;
+    if (servidor == 1) colorEmbeded = 0x00BECA;
+    ops.buscaLinks(ops.formataMsgBusca(nomeMonstro), servidor).then(async function(links) {
+        for (const link of links) {
+          await ops.buscaDetalhesMstr(link.url)
+          .then(function(monstro) {
+            const embed = new discord.RichEmbed()
+                .setTitle(monstro.nome)
+                .setColor(colorEmbeded)
+                .setDescription('montar mensagem')
+            return message.channel.send(embed);
+            }, err => console.log("Error:" + err)
+          ).catch(e => console.log("Error:" + e));
+        }
+      }, err => console.log("Error:" + err)
+    ).catch(e => console.log("Error:" + e));
+}
